fd881266d95c12543dfeaae7464df7b09819d0f8
tidynoob <mitchellgriffin9@gmail.com>
2022-10-05 01:22:16 -0400

    Add initial functions

--- src/loadHome.js
+++ src/loadHome.js
@@ -1,20 +1,22 @@
 import meow from './meow.jpg';
+var fp = require('lodash/fp');
+
+const div = document.getElementById('content');
+div.classList.add('ui','container');
+const img = new Image();
+img.src = meow;
+const h1 = document.createElement('h1');
+h1.textContent = 'Meow Meows';
+const h2 = document.createElement('h2');
+h2.textContent = 'The Snackiest Snacks';
+const p = document.createElement('p');
+p.textContent = 'Our treats will keep you purring for more!';
+
+const appendToDiv = fp.bind(div.appendChild)(div);
 
 const loadHome = () => {
-    const div = document.getElementById('content');
-    const img = new Image();
-    img.src = meow;
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Meow Meows';
-    const h2 = document.createElement('h2');
-    h2.textContent = 'The Snackiest Snacks';
-    const p = document.createElement('p');
-    p.textContent = 'Our treats will keep you purring for more!';
-    div.appendChild(img);
-    div.appendChild(h2);
-    div.appendChild(p);
-    div.classList.add('ui','container');
 
+    fp.forEach(appendToDiv)([img,h1, h2, p]);
 }
 
 export default loadHome;
--- src/nav.js
+++ src/nav.js
@@ -1,34 +1,64 @@
 import logo from './cat.svg';
+var fp = require('lodash/fp');
 
+const navClasses = ['navbar', 'py-3', 'mb-4', 'border-bottom'];
+const containerClasses = ['container'];
+const aClasses = ['navbar-brand', 'd-flex', 'align-items-center', 'gap-1'];
+const imgClasses = ['align-middle'];
+const spanClasses = ['fs-4'];
+const ulClasses = ['nav','nav-pills','gap-3'];
+const tabs = ['home','menu','contact'];
+const tabClasses = ['nav-item'];
+const content = document.getElementById('content');
+
+const nav = document.createElement('nav');
+nav.classList.add(...navClasses);
+
+const container = document.createElement('div');
+container.classList.add(...containerClasses);
+
+const a = document.createElement('a');
+a.classList.add(...aClasses);
+a.href = '#';
+
+const myImg = new Image();
+myImg.src = logo;
+myImg.classList.add(imgClasses);
+
+const span = document.createElement('span');
+span.classList.add(...spanClasses);
+span.textContent = 'Cat Cafe';
+
+a.appendChild(myImg);
+a.appendChild(span);
+
+const ul = document.createElement('ul');
+ul.classList.add(...ulClasses);
+
+tabs.forEach(tab => {
+    // console.log(tab);
+    const li = document.createElement('li');
+    li.classList.add(...tabClasses);
+    const a = document.createElement('a');
+    if (tab === 'home') {
+        a.classList.add('active','nav-link');
+    }
+    a.textContent = tab;
+    a.href = '#';
+    li.appendChild(a);
+    ul.appendChild(li);
+    // console.log(ul);
+});
+
+container.appendChild(a);
+container.appendChild(ul);
+nav.appendChild(container);
+
+const bindInsert = fp.bind(content.parentNode.insertBefore)(content.parentNode);
+const insertBeforeContent = fp.curryRight(bindInsert)(content);
 
 const loadNav = () => {
-    const content = document.getElementById('content');
-
-    const nav = document.createElement('nav');
-    nav.classList.add('navbar', 'navbar-light' , 'bg-light');
-
-    const container = document.createElement('div');
-    container.classList.add('container-fluid');
-
-    const a = document.createElement('a');
-    a.classList.add('navbar-brand','d-flex','align-items-center','gap-1');
-    a.href = '#';
-
-    const myImg = new Image();
-    myImg.src = logo;
-    myImg.width = 30;
-    myImg.classList.add('align-middle');
-
-    const span = document.createElement('span');
-    span.textContent = 'Cat Cafe';
-
-    a.appendChild(myImg);
-    a.appendChild(span);
-
-    container.appendChild(a);
-    nav.appendChild(container);
-    content.parentNode.insertBefore(nav, content);
-
+    insertBeforeContent(nav);
 }
 
 export default loadNav;
